52f930c885cc9b1e2f9df38786845b5d6d0b498f
Mikołaj Pasiut <s498782@labs.wmi.amu.edu.pl>
2026-05-12 16:01:41 +0200

    Parser start

--- parser.py
+++ parser.py
@@ -1,11 +1,38 @@
 import time
 from lark import Lark
 
+#{int}d{int}
+
+
+
 class Parser():
     def __init__(self):
-        pass
+        grammar = """
+            statement: modifier_back? n_rolls dice n_faces modifier_front?
+
+            modifier_front: add_front | sub_front 
+            modifier_back: add_back | sub_back
+
+            add_front: "+" SIGNED_NUMBER
+            add_back: SIGNED_NUMBER "+"
+            sub_front: "-" SIGNED_NUMBER
+            sub_back: SIGNED_NUMBER "-"
+
+            n_rolls: SIGNED_NUMBER?
+            dice: "d"
+            n_faces: SIGNED_NUMBER
+
+            %import common.WS
+            %import common.SIGNED_NUMBER
+            %ignore WS
+        """
+        self.x = Lark(grammar, start="statement")
 
     def parse(self, statement):
-        print("Thinking...")
-        time.sleep(5)
-        print(f"{statement} is not a valid dice notation")
+        statement = statement.replace(" ", "")
+        print(self.x.parse(statement).pretty())
+        st_lexed = self.lexer(statement)
+        return st_lexed
+
+    def lexer(self, statement):
+        pass
