6d9f861290b3260e8821481997415d73beeff905
Mikołaj Pasiut <s498782@labs.wmi.amu.edu.pl>
2026-05-14 12:56:18 +0200

    parser improvements

--- parser.py
+++ parser.py
@@ -1,28 +1,18 @@
-import time
 from lark import Lark
-
-#{int}d{int}
-
-
 
 class Parser():
     def __init__(self):
         grammar = """
-            statement: modifier* (dice_throw_sub|dice_throw_add)+ modifier*
+            statement: (modifier (function)* operator)? dice_throw ((operator dice_throw)|function)*
+            dice_throw: n_rolls "d" n_faces
+            
+            minus: /-/
+            mod_num: NUMBER
 
-            modifier_front: add_front | sub_front 
-            modifier_back: add_back | sub_back
+            modifier: minus? mod_num
+            function: operator modifier
 
-            dice_throw_sub: "-" n_rolls "d" n_faces
-            dice_throw_add: "+" n_rolls "d" n_faces
-
-            add_front: "+" SIGNED_NUMBER
-            add_back: SIGNED_NUMBER "+"
-            sub_front: "-" SIGNED_NUMBER
-            sub_back: SIGNED_NUMBER "-"
-
-            sint: ["+"|"-"]
-            modifier: SIGNED_NUMBER
+            operator: /(\\*\\*)|[-+*\\/]/
 
 
             n_rolls: INT?
@@ -31,12 +21,12 @@ class Parser():
             %import common.WS
             %import common.INT
             %import common.SIGNED_NUMBER
+            %import common.NUMBER
             %ignore WS
         """
         self.x = Lark(grammar, start="statement")
 
     def parse(self, statement):
-        statement = statement.replace(" ", "")
         print(self.x.parse(statement).pretty())
         st_lexed = self.lexer(statement)
         return st_lexed
